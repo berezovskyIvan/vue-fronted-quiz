1d0228d55e4b05d0872b201a860b5f37b7bf72cd
i.berezovskiy <i.berezovskiy@kt-team.de>
2020-05-26 23:33:13 +0300

    Создана первая версия проекта VueJS
    1. Проведена установка библиотек для работы со стилями
    2. Настроен web pack для сборки проекта
    3. Настроена VueJS архитектура

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  #app
+    div ДОБРО ПОЖАЛОВАТЬ
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
