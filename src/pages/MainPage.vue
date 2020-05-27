07860a08472e77bdbd0550cc0a8647d946086ad3
i.berezovskiy <i.berezovskiy@kt-team.de>
2020-05-28 00:04:16 +0300

    К проекту подключен vue router. Настроена маршрутизация
    Разработаны шаблоны страниц для аутентификации, не найденной страницы и главной страницы.

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .main-page
+    h1 WELCOME TO PROJECT
+</template>
+
+<script>
+export default {
+  name: 'MainPage'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/global';
+
+.main-page {
+  @include flex-center;
+
+  h1 {
+    font-size: 30px;
+  }
+}
+</style>
